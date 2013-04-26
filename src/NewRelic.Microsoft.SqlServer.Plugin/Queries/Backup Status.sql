1650c96c12a02a6a8e5c0065d443de7bacac22cb
Mike Merrill <mmerrill@crosscurrentinc.com>
2013-04-26 10:48:14 -0700

    Log and Full backup checks. Pir chart with drill down

---
+++ src/NewRelic.Microsoft.SqlServer.Plugin/Queries/Backup Status.sql
@@ -0,0 +1,24 @@
+SELECT
+	SUBSTRING(s.name, 1, 40) AS 'Database',
+	CAST(b.backup_start_date AS char(11)) AS 'Backup Date  ',
+	CASE
+			WHEN b.backup_start_date > DATEADD(dd, -1, GETDATE()) THEN 'Backup is current within a day' 
+			ELSE 'Backup is NOT current within a day'
+	END	AS 'Comment',
+	CASE
+			WHEN b.backup_start_date > DATEADD(dd, -1, GETDATE()) THEN 'OK'
+			ELSE 'ERROR'
+	END	AS 'Flag'
+
+FROM master..sysdatabases s
+LEFT OUTER JOIN msdb..backupset b
+	ON s.name = b.database_name
+	AND b.backup_start_date = (SELECT
+		MAX(backup_start_date)
+	FROM msdb..backupset
+	WHERE database_name = b.database_name
+	AND type = 'D') -- full database backups only, not log backups
+WHERE s.name <> 'tempdb'
+
+
+
