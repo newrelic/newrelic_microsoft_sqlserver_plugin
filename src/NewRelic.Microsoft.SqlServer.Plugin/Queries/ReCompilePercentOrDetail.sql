e81d9960f8599b3d3be68e6a99852405218342c3
Mike Merrill <mmerrill@crosscurrentinc.com>
2013-04-27 11:42:19 -0700

    Recompiles

--- src/NewRelic.Microsoft.SqlServer.Plugin/Queries/ReCompilePercentOrDetail.sql
+++ src/NewRelic.Microsoft.SqlServer.Plugin/Queries/ReCompilePercentOrDetail.sql
@@ -1,45 +1,53 @@
-declare @SingleUse int, @MultiUse int, @DetailSummary varchar(1)
+DECLARE	@SingleUse int,
+		@MultiUse int,
+		@DetailSummary varchar(1)
 
-set @DetailSummary = 'S'
 
-declare @Temp1 Table(
-	DBName Varchar(200) NULL,
+
+SET @DetailSummary = 'S'
+
+
+
+DECLARE @Temp1 TABLE (
+	DBName varchar(200) NULL,
 	bucketid bigint NULL,
 	usecounts bigint NULL,
 	size_in_bytes bigint NULL,
 	objtype varchar(25) NULL,
-	SQLtext varchar(max) NULL
+	SQLtext varchar(MAX) NULL
 )
 
-insert into @Temp1
-    select
-        db_name(st.dbid) as DBName,
-        cp.bucketid,
-        cp.usecounts,
-        cp.size_in_bytes,
-        cp.objtype,
-        st.text
-    from sys.dm_exec_cached_plans cp
-    cross apply sys.dm_exec_sql_text(cp.plan_handle) st
-    where cp.cacheobjtype = 'Compiled Plan'
+INSERT INTO @Temp1
+	SELECT
+		DB_NAME(st.dbid) AS DBName,
+		cp.bucketid,
+		cp.usecounts,
+		cp.size_in_bytes,
+		cp.objtype,
+		st.text
+	FROM sys.dm_exec_cached_plans cp
+	CROSS APPLY sys.dm_exec_sql_text(cp.plan_handle) st
+	WHERE cp.cacheobjtype = 'Compiled Plan'
 
-select @SingleUse = count(*)
-from @Temp1
-where usecounts = 1
+SELECT
+	@SingleUse = COUNT(*)
+FROM @Temp1
+WHERE usecounts = 1
 
-select @MultiUse = count(*)
-from @Temp1
-where usecounts > 1
+SELECT
+	@MultiUse = COUNT(*)
+FROM @Temp1
+WHERE usecounts > 1
 
-if @DetailSummary = 'S'
-select
-    @SingleUse as SingleUses,
-    @MultiUse as MultiUses,
-    cast(@SingleUse as decimal(18,2)) / (@SingleUse + @MultiUse) * 100
-        as SingleUsePercent
-        
+IF @DetailSummary = 'S'
+	SELECT
+		@SingleUse AS SingleUses,
+		@MultiUse AS MultiUses,
+		CAST(@SingleUse AS decimal(18, 2)) / (@SingleUse + @MultiUse) * 100
+		AS SingleUsePercent
 
-if @DetailSummary = 'D'
-    select
-	*
-	from @Temp1
+
+IF @DetailSummary = 'D'
+	SELECT
+		*
+	FROM @Temp1
