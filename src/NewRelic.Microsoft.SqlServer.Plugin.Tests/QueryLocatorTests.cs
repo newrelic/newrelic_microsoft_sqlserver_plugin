cfc0032b53010d51ffa58befdab781446f4b9c75
Jesse Stromwick <jstromwick@gmail.com>
2013-04-30 23:23:35 -0700

    Fixing up test for QueryLocator to demonstrate that QueryLocator will throw an argument exception if a query does not implement IQueryResult.

--- src/NewRelic.Microsoft.SqlServer.Plugin.Tests/QueryLocatorTests.cs
+++ src/NewRelic.Microsoft.SqlServer.Plugin.Tests/QueryLocatorTests.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using NSubstitute;
@@ -11,136 +10,150 @@ using NewRelic.Platform.Binding.DotNET;
 
 namespace NewRelic.Microsoft.SqlServer.Plugin
 {
-	[TestFixture]
-	public class QueryLocatorTests
-	{
-		[SqlMonitorQuery("NewRelic.Microsoft.SqlServer.Plugin.Core.ExampleEmbeddedFile.sql")]
-		private class QueryTypeWithExactResourceName : FakeQueryResultBase {}
+    [TestFixture]
+    public class QueryLocatorTests
+    {
+        [SqlMonitorQuery("NewRelic.Microsoft.SqlServer.Plugin.Core.ExampleEmbeddedFile.sql")]
+        private class QueryTypeWithExactResourceName : FakeQueryResultBase {}
 
-		[SqlMonitorQuery("Queries.ExampleEmbeddedFile.sql")]
-		private class QueryTypeWithPartialResourceName : FakeQueryResultBase {}
+        [SqlMonitorQuery("Queries.ExampleEmbeddedFile.sql")]
+        private class QueryTypeWithPartialResourceName : FakeQueryResultBase {}
 
-		[SqlMonitorQuery("AnotherQuery.sql")]
-		private class QueryTypeWithJustFileName : FakeQueryResultBase {}
+        [SqlMonitorQuery("AnotherQuery.sql")]
+        private class QueryTypeWithJustFileName : FakeQueryResultBase {}
 
-		[SqlMonitorQuery("AnotherQuery.sql")]
-		[SqlMonitorQuery("Queries.ExampleEmbeddedFile.sql")]
-		private class QueryTypeWithTwoQueries : FakeQueryResultBase {}
+        [SqlMonitorQuery("AnotherQuery.sql")]
+        [SqlMonitorQuery("Queries.ExampleEmbeddedFile.sql")]
+        private class QueryTypeWithTwoQueries : FakeQueryResultBase {}
 
-		[SqlMonitorQuery("Foo.sql", Enabled = false)]
-		private class QueryTypeDisabled : FakeQueryResultBase {}
+        [SqlMonitorQuery("Foo.sql", Enabled = false)]
+        private class QueryTypeDisabled : FakeQueryResultBase {}
 
-		[SqlMonitorQuery("Foo.sql", Enabled = false)]
-		[SqlMonitorQuery("AnotherQuery.sql", QueryName = "This is enabled")]
-		private class QueryTypeSomeEnabled : FakeQueryResultBase {}
+        [SqlMonitorQuery("Foo.sql", Enabled = false)]
+        [SqlMonitorQuery("AnotherQuery.sql", QueryName = "This is enabled")]
+        private class QueryTypeSomeEnabled : FakeQueryResultBase {}
 
-		private class FakeQueryResultBase : IQueryResult
-		{
-			public void AddMetrics(ComponentData componentData) {}
-		}
+        [SqlMonitorQuery("AnotherQuery.sql", QueryName = "This is enabled")]
+        private class QueryThatIsNotIQueryResult {}
 
-		[Test]
-		public void Assert_funcs_are_correctly_configured()
-		{
-			var dapperWrapper = Substitute.For<IDapperWrapper>();
+        private class FakeQueryResultBase : IQueryResult
+        {
+            public void AddMetrics(ComponentData componentData) {}
+        }
 
-			var queries = new QueryLocator(dapperWrapper).PrepareQueries();
-			foreach (var query in queries)
-			{
-				var results = query.Invoke(null);
-				Assert.That(results, Is.EqualTo(new object[0]));
-			}
-		}
 
-		[Test]
-		public void Assert_multiple_query_attributes_yield_multiple_queries()
-		{
-			var assembly = Assembly.GetExecutingAssembly();
-			var queryLocator = new QueryLocator(null, assembly);
+        [Test]
+        public void Assert_funcs_are_correctly_configured()
+        {
+            var dapperWrapper = Substitute.For<IDapperWrapper>();
 
-			var queries = queryLocator.PrepareQueries(new[] {typeof (QueryTypeWithTwoQueries)});
-			Assert.That(queries, Is.Not.Null);
-			var queryNames = queries.Select(q => q.ResourceName).ToArray();
-			var expected = new[] {"AnotherQuery.sql", "Queries.ExampleEmbeddedFile.sql"};
-			Assert.That(queryNames, Is.EquivalentTo(expected));
-		}
+            var queries = new QueryLocator(dapperWrapper).PrepareQueries();
+            foreach (var query in queries)
+            {
+                var results = query.Invoke(null);
+                Assert.That(results, Is.EqualTo(new object[0]));
+            }
+        }
 
-		[Test]
-		public void Assert_resource_with_exact_resource_name_is_located()
-		{
-			var assembly = Assembly.GetExecutingAssembly();
-			var queryLocator = new QueryLocator(null, assembly);
+        [Test]
+        public void Assert_multiple_query_attributes_yield_multiple_queries()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var queryLocator = new QueryLocator(null, assembly);
 
-			var queries = queryLocator.PrepareQueries(new[] {typeof (QueryTypeWithExactResourceName)});
-			Assert.That(queries, Is.Not.Null);
-			var queryNames = queries.Select(q => q.ResultTypeName).ToArray();
-			Assert.That(queryNames, Is.EqualTo(new[] {typeof (QueryTypeWithExactResourceName).Name}));
-		}
+            var queries = queryLocator.PrepareQueries(new[] {typeof (QueryTypeWithTwoQueries)});
+            Assert.That(queries, Is.Not.Null);
+            var queryNames = queries.Select(q => q.ResourceName).ToArray();
+            var expected = new[] {"AnotherQuery.sql", "Queries.ExampleEmbeddedFile.sql"};
+            Assert.That(queryNames, Is.EquivalentTo(expected));
+        }
 
-		[Test]
-		public void Assert_resource_with_only_file_name_is_located()
-		{
-			var assembly = Assembly.GetExecutingAssembly();
-			var queryLocator = new QueryLocator(null, assembly);
+        [Test]
+        [ExpectedException(typeof (ArgumentException))]
+        public void Assert_queries_not_IQueryResult_throws_exception()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var queryLocator = new QueryLocator(null, assembly);
 
-			var queries = queryLocator.PrepareQueries(new[] {typeof (QueryTypeWithJustFileName)});
-			Assert.That(queries, Is.Not.Null);
-			var queryNames = queries.Select(q => q.ResultTypeName).ToArray();
-			Assert.That(queryNames, Is.EqualTo(new[] {typeof (QueryTypeWithJustFileName).Name}));
-		}
+            queryLocator.PrepareQueries(new[] {typeof (QueryThatIsNotIQueryResult)});
+        }
 
-		[Test]
-		public void Assert_resource_with_partial_resource_name_is_located()
-		{
-			var assembly = Assembly.GetExecutingAssembly();
-			var queryLocator = new QueryLocator(null, assembly);
+        [Test]
+        public void Assert_resource_with_exact_resource_name_is_located()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var queryLocator = new QueryLocator(null, assembly);
 
-			var queries = queryLocator.PrepareQueries(new[] {typeof (QueryTypeWithPartialResourceName)});
-			Assert.That(queries, Is.Not.Null);
-			var queryNames = queries.Select(q => q.ResultTypeName).ToArray();
-			Assert.That(queryNames, Is.EqualTo(new[] {typeof (QueryTypeWithPartialResourceName).Name}));
-		}
+            var queries = queryLocator.PrepareQueries(new[] {typeof (QueryTypeWithExactResourceName)});
+            Assert.That(queries, Is.Not.Null);
+            var queryNames = queries.Select(q => q.ResultTypeName).ToArray();
+            Assert.That(queryNames, Is.EqualTo(new[] {typeof (QueryTypeWithExactResourceName).Name}));
+        }
 
-		[Test]
-		public void Assert_some_query_types_are_found()
-		{
-			var assembly = Assembly.GetExecutingAssembly();
+        [Test]
+        public void Assert_resource_with_only_file_name_is_located()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var queryLocator = new QueryLocator(null, assembly);
 
-			var types = assembly.GetTypes();
-			Assume.That(types, Is.Not.Empty, "Expected at least one type in the test assembly");
+            var queries = queryLocator.PrepareQueries(new[] {typeof (QueryTypeWithJustFileName)});
+            Assert.That(queries, Is.Not.Null);
+            var queryNames = queries.Select(q => q.ResultTypeName).ToArray();
+            Assert.That(queryNames, Is.EqualTo(new[] {typeof (QueryTypeWithJustFileName).Name}));
+        }
 
-			var typesWithAttribute = types.Where(t => t.GetCustomAttributes<SqlMonitorQueryAttribute>().Any());
-			Assert.That(typesWithAttribute, Is.Not.Empty, "Expected at least one QueryType using the " + typeof (SqlMonitorQueryAttribute).Name);
-		}
+        [Test]
+        public void Assert_resource_with_partial_resource_name_is_located()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var queryLocator = new QueryLocator(null, assembly);
 
-		[Test]
-		public void Assert_that_disabled_queries_are_ignored()
-		{
-			var assembly = Assembly.GetExecutingAssembly();
-			var queryLocator = new QueryLocator(null, assembly);
+            var queries = queryLocator.PrepareQueries(new[] {typeof (QueryTypeWithPartialResourceName)});
+            Assert.That(queries, Is.Not.Null);
+            var queryNames = queries.Select(q => q.ResultTypeName).ToArray();
+            Assert.That(queryNames, Is.EqualTo(new[] {typeof (QueryTypeWithPartialResourceName).Name}));
+        }
 
-			var queries = queryLocator.PrepareQueries(new[] {typeof (QueryTypeDisabled)});
-			Assert.That(queries, Is.Empty);
-		}
+        [Test]
+        public void Assert_some_query_types_are_found()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
 
-		[Test]
-		public void Assert_that_only_enabled_queries_are_found()
-		{
-			var assembly = Assembly.GetExecutingAssembly();
-			var queryLocator = new QueryLocator(null, assembly);
+            var types = assembly.GetTypes();
+            Assume.That(types, Is.Not.Empty, "Expected at least one type in the test assembly");
 
-			var queries = queryLocator.PrepareQueries(new[] {typeof (QueryTypeSomeEnabled)})
-			                          .Select(q => q.QueryName)
-			                          .ToArray();
+            var typesWithAttribute = types.Where(t => t.GetCustomAttributes<SqlMonitorQueryAttribute>().Any());
+            Assert.That(typesWithAttribute, Is.Not.Empty, "Expected at least one QueryType using the " + typeof (SqlMonitorQueryAttribute).Name);
+        }
 
-			Assert.That(queries, Is.EqualTo(new[] {"This is enabled"}));
-		}
+        [Test]
+        public void Assert_that_disabled_queries_are_ignored()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var queryLocator = new QueryLocator(null, assembly);
 
-		[Test]
-		public void Assert_that_queries_are_located()
-		{
-			var queries = new QueryLocator(new DapperWrapper(), Assembly.GetExecutingAssembly()).PrepareQueries();
-			Assert.That(queries, Is.Not.Empty, "Expected some queries to be returned");
-		}
-	}
+            var queries = queryLocator.PrepareQueries(new[] {typeof (QueryTypeDisabled)});
+            Assert.That(queries, Is.Empty);
+        }
+
+        [Test]
+        public void Assert_that_only_enabled_queries_are_found()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var queryLocator = new QueryLocator(null, assembly);
+
+            var queries = queryLocator.PrepareQueries(new[] {typeof (QueryTypeSomeEnabled)})
+                                      .Select(q => q.QueryName)
+                                      .ToArray();
+
+            Assert.That(queries, Is.EqualTo(new[] {"This is enabled"}));
+        }
+
+        [Test]
+        public void Assert_that_queries_are_located()
+        {
+            var queries = new QueryLocator(new DapperWrapper(), Assembly.GetExecutingAssembly(), new[] {typeof (QueryThatIsNotIQueryResult)}).PrepareQueries();
+            Assert.That(queries, Is.Not.Empty, "Expected some queries to be returned");
+        }
+    }
 }
