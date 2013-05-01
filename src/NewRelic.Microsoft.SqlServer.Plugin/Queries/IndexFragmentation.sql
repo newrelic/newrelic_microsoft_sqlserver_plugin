d5f02d6b878d261e9645961bc463fe2ad8cc95f4
Mike Merrill <mmerrill@crosscurrentinc.com>
2013-05-01 11:51:18 -0700

    Index Frag

---
+++ src/NewRelic.Microsoft.SqlServer.Plugin/Queries/IndexFragmentation.sql
@@ -0,0 +1,20 @@
+-- Index Fragmentation 
+-- Returns DB, Related Table, Fragmentation and index name for index objects with fragmentation over @FragPercent
+-- This could take 2 - 15 minutes to run on a server with many large DBs. This returns important information, so an execution strategy is needed.
+
+
+DECLARE @FragPercent int
+SET @FragPercent = 10
+
+SELECT
+	DB_NAME(pis.database_id) AS DBName,
+	OBJECT_NAME(pis.object_id) AS TableName,
+	si.name AS ObjectName,
+	pis.avg_fragmentation_in_percent
+FROM sys.dm_db_index_physical_stats(NULL, NULL, NULL, NULL, 'LIMITED') pis
+JOIN sysindexes si
+	ON pis.object_id = si.id
+	AND
+	pis.index_id = si.indid
+WHERE pis.avg_fragmentation_in_percent > @FragPercent
+ORDER BY pis.avg_fragmentation_in_percent DESC
