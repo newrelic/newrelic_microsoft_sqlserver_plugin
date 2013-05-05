3d6e7a4b4db0f240617109f2928c0423b9bc2aa4
Ed Chapel <edchapel@gmail.com>
2013-05-05 09:48:20 -0700

    Implemented MemoryView query

--- src/NewRelic.Microsoft.SqlServer.Plugin/Queries/MemoryView.sql
+++ src/NewRelic.Microsoft.SqlServer.Plugin/Queries/MemoryView.sql
@@ -2,21 +2,19 @@
 -- Returns ratio of memory cache hits to non-memory cache hits
 -- Returns memory page life expectancy for entire instance for NUMA and non-NUMA processor/memory configs
 SELECT
-	(SELECT
-		cntr_value
-	FROM sys.dm_os_performance_counters
-	WHERE counter_name = 'Buffer cache hit ratio') / (SELECT
-														cntr_value
-														FROM sys.dm_os_performance_counters
-														WHERE counter_name = 'Buffer cache hit ratio base')
-	* 100.00 AS 'BufferCacheHitRatio',
-	(SELECT
-		cntr_value
-	FROM sys.dm_os_performance_counters
-	WHERE counter_name = 'Page life expectancy' AND [OBJECT_NAME] = 'SQLServer:Buffer Manager')
-	AS 'PageLifeExpectancyInSeconds',
-	(SELECT
-		cntr_value
-	FROM sys.dm_os_performance_counters
-	WHERE counter_name = 'Page life expectancy' AND [OBJECT_NAME] = 'SQLServer:Buffer Node')
-	AS 'PageLifeExpectancyInSeconds-NUMA'
+	(
+		SELECT cntr_value
+		FROM sys.dm_os_performance_counters
+		WHERE counter_name = 'Buffer cache hit ratio'
+	) / (
+		SELECT cntr_value
+		FROM sys.dm_os_performance_counters
+		WHERE counter_name = 'Buffer cache hit ratio base') AS BufferCacheHitRatio,
+	(
+		SELECT cntr_value
+		FROM sys.dm_os_performance_counters
+		WHERE counter_name = 'Page life expectancy' AND [OBJECT_NAME] = 'SQLServer:Buffer Manager') AS PageLifeExpectancyInSeconds,
+	(
+		SELECT cntr_value
+		FROM sys.dm_os_performance_counters
+		WHERE counter_name = 'Page life expectancy' AND [OBJECT_NAME] = 'SQLServer:Buffer Node') AS PageLifeExpectancyInSecondsNuma
--- src/NewRelic.Microsoft.SqlServer.Plugin/QueryTypes/DatabaseMetricBase.cs
+++ src/NewRelic.Microsoft.SqlServer.Plugin/QueryTypes/DatabaseMetricBase.cs
@@ -3,53 +3,52 @@ using System.Linq;
 
 namespace NewRelic.Microsoft.SqlServer.Plugin.QueryTypes
 {
-    internal abstract class DatabaseMetricBase : IDatabaseMetric
-    {
-        protected abstract WhereClauseTokenEnum WhereClauseToken { get; }
+	internal abstract class DatabaseMetricBase : IDatabaseMetric
+	{
+		protected abstract WhereClauseTokenEnum WhereClauseToken { get; }
+		protected abstract string DbNameForWhereClause { get; }
+		public string DatabaseName { get; set; }
 
-        protected abstract string DbNameForWhereClause { get; }
-        public string DatabaseName { get; set; }
+		public string ParameterizeQuery(string commandText, string[] includeDBs, string[] excludeDBs)
+		{
+			string whereToken;
+			string where;
+			switch (WhereClauseToken)
+			{
+				case WhereClauseTokenEnum.Where:
+					whereToken = Constants.WhereClauseReplaceToken;
+					where = "WHERE";
+					break;
+				case WhereClauseTokenEnum.WhereAnd:
+					whereToken = Constants.WhereClauseAndReplaceToken;
+					where = "AND";
+					break;
+				case WhereClauseTokenEnum.Unknown:
+				default:
+					throw new ArgumentException(string.Format("WhereClauseToken '{0}' not recognized. ",
+					                                          WhereClauseToken));
+			}
 
-        public string ParameterizeQuery(string commandText, string[] includeDBs, string[] excludeDBs)
-        {
-            string whereToken;
-            string where;
-            switch (WhereClauseToken)
-            {
-                case WhereClauseTokenEnum.Where:
-                    whereToken = Constants.WhereClauseReplaceToken;
-                    where = "WHERE";
-                    break;
-                case WhereClauseTokenEnum.WhereAnd:
-                    whereToken = Constants.WhereClauseAndReplaceToken;
-                    where = "AND";
-                    break;
-                case WhereClauseTokenEnum.Unknown:
-                default:
-                    throw new ArgumentException(string.Format("WhereClauseToken '{0}' not recognized. ",
-                                                              WhereClauseToken));
-            }
+			if (!commandText.Contains(whereToken))
+			{
+				throw new Exception(string.Format("SQL is not in the expected format. Missing replacement token '{0}'", WhereClauseToken));
+			}
 
-            if (!commandText.Contains(whereToken))
-            {
-                throw new Exception(string.Format("SQL is not in the expected format. Missing replacement token '{0}'", WhereClauseToken));
-            }
+			if (includeDBs != null && includeDBs.Any())
+			{
+				var dbNames = includeDBs.Select(d => string.Format("'{0}'", d)).ToArray();
+				var replacement = string.Format("{0} ({1} IN ({2}))", where, DbNameForWhereClause, string.Join(", ", dbNames));
+				return commandText.Replace(whereToken, replacement);
+			}
 
-            if (includeDBs != null && includeDBs.Any())
-            {
-                var dbNames = includeDBs.Select(d => String.Format("'{0}'", d)).ToArray();
-                var replacement = string.Format("{0} ({1} IN ({2}))", where, DbNameForWhereClause, string.Join(", ", dbNames));
-                return commandText.Replace(whereToken, replacement);
-            }
+			if (excludeDBs != null && excludeDBs.Any())
+			{
+				var dbNames = excludeDBs.Select(d => string.Format("'{0}'", d)).ToArray();
+				var replacement = string.Format("{0} ({1} NOT IN ({2}))", where, DbNameForWhereClause, string.Join(", ", dbNames));
+				return commandText.Replace(whereToken, replacement);
+			}
 
-            if (excludeDBs != null && excludeDBs.Any())
-            {
-                var dbNames = excludeDBs.Select(d => string.Format("'{0}'", d)).ToArray();
-                var replacement = string.Format("{0} ({1} NOT IN ({2}))", where, DbNameForWhereClause, string.Join(", ", dbNames));
-                return commandText.Replace(whereToken, replacement);
-            }
-
-            return commandText;
-        }
-    }
+			return commandText;
+		}
+	}
 }
