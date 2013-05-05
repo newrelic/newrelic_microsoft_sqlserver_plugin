32af3cc4f85bfb1675fccf0fe917311235f2eca2
Ed Chapel <edchapel@gmail.com>
2013-05-05 16:02:51 -0700

    Clean up build script for alpha release Clean app.config to match install instructions

--- src/Common/CommonAssemblyInfo.cs
+++ src/Common/CommonAssemblyInfo.cs
@@ -7,6 +7,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0.0+0000000")]
+[assembly: AssemblyInformationalVersion("0.1.0+alpha")]
