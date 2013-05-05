99f9cc0ae40fd3d434889bd924efc7e6921fd3fe
Ed Chapel <edchapel@gmail.com>
2013-05-05 14:02:50 -0700

    Added command line build script including test run and package step

--- src/NewRelic.Microsoft.SqlServer.Plugin/Properties/AssemblyInfo.cs
+++ src/NewRelic.Microsoft.SqlServer.Plugin/Properties/AssemblyInfo.cs
@@ -5,16 +5,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NewRelic Microsoft SqlServer Plugin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("NewRelic.Microsoft.SqlServer.Plugin")]
-[assembly: AssemblyProduct("NewRelic.Microsoft.SqlServer.Plugin")]
-[assembly: AssemblyCopyright("Copyright © New Relic 2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
 [assembly: Guid("78b520e8-a393-4c1b-bf14-b535b8d111be")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0.0+0000000")]
 
 #if DEBUG
 [assembly: InternalsVisibleTo("NewRelic.Microsoft.SqlServer.Plugin.Tests")]
