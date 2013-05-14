331ea022088a1f0f2121ef13aef5734261858db7
Ed Chapel <edchapel@gmail.com>
2013-05-13 23:55:41 -0700

    Changed build script to also update the AssemblyFileVersion

--- src/Common/CommonAssemblyInfo.cs
+++ src/Common/CommonAssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("0.3.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.3.0")]
 [assembly: AssemblyInformationalVersion("0.3.0+alpha")]
