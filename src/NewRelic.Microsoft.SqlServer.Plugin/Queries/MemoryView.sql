a08b68ae432fe01a793aa812d3ae67aa59ca73af
Ed Chapel <edchapel@gmail.com>
2013-05-23 10:49:03 -0700

    Fixed non-NUMA query bug that crept in late last night. Fixes #3.

--- src/NewRelic.Microsoft.SqlServer.Plugin/Queries/MemoryView.sql
+++ src/NewRelic.Microsoft.SqlServer.Plugin/Queries/MemoryView.sql
@@ -4,18 +4,18 @@
 -- Data collection nature: Cumulative. But this might be just fine for trending. Note: Server resets will zero out the page lifes.
 SELECT
 	(SELECT
-			cntr_value * 100.00
-		FROM sys.dm_os_performance_counters
-		WHERE counter_name = 'Buffer cache hit ratio')
+		cntr_value * 100.00
+	FROM sys.dm_os_performance_counters
+	WHERE counter_name = 'Buffer cache hit ratio')
 	/ (SELECT
-			cntr_value
-		FROM sys.dm_os_performance_counters
-		WHERE counter_name = 'Buffer cache hit ratio base')
+		cntr_value
+	FROM sys.dm_os_performance_counters
+	WHERE counter_name = 'Buffer cache hit ratio base')
 	AS BufferCacheHitRatio,
 	(SELECT
-			cntr_value
-		FROM sys.dm_os_performance_counters
-		WHERE counter_name = 'Page life expectancy'
-		-- The OBJECT_NAME is fixed width with many trailing spaces
-		AND RTRIM([OBJECT_NAME]) LIKE '%Buffer Node')
+		cntr_value
+	FROM sys.dm_os_performance_counters
+	WHERE counter_name = 'Page life expectancy'
+	-- The OBJECT_NAME is fixed width with many trailing spaces
+	AND RTRIM([object_name]) LIKE '%:Buffer Manager')
 	AS PageLife
