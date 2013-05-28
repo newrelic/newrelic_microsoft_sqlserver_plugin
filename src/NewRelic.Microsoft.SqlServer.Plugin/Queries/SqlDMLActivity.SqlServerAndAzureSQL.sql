e5fe19d91146ed2a82ac20959914446a271459b6
Mike Merrill <mmerrill@crosscurrentinc.com>
2013-05-28 14:42:03 -0700

    Lots of work on DML load. Dealt with corner cases. Platform issues. False positives. Encryption issues.

--- src/NewRelic.Microsoft.SqlServer.Plugin/Queries/SqlDMLActivity.SqlServerAndAzureSQL.sql
+++ src/NewRelic.Microsoft.SqlServer.Plugin/Queries/SqlDMLActivity.SqlServerAndAzureSQL.sql
@@ -1,11 +1,21 @@
--- DML Activity
--- SQL and Azure
+-- DML Activity per SQL statement
+-- SQL and Azure: SQL is for all DBs on instance. Azure is for DB context. (SQL: DBID is NULL for ad hoc and prepared SQL statements)
 -- Feeds a graph to show normal data access in SQL by read and write statements
+-- Unable to use query hash. It is not availible in SQL Server 2005.
+-- creation_time tells you the date and time that the plan that the query is part of was compiled. The stats are all as of the plan and creation_time.
+-- So, we need to track SQL statements in plans and add up the stats since compile time.
+-- Encrypted SQL statements are ignored
+-- We default to a Read when the statement is not categorized. (rare)
+-- Use: 
+	--plan_handle, SQLStatement and creation_time will give a unique item to calc delta. 
+	--These stay in the result set until they un-cache. 
+	--Un-cached items count as a 0 delta. 
+	--Drop these from delta tracking list.
+
 
 
 declare @SqlText as table(
-	sql_handle varbinary(64),
-	query_hash varbinary(max),
+	plan_handle varbinary(64),
 	SqlStatement nvarchar(max),
 	creation_time DATETIME,
 	execution_count INT,
@@ -17,18 +27,16 @@ declare @SqlText as table(
 
 INSERT INTO @SqlText		
 SELECT 
-	queryData.sql_handle,
-	queryData.query_hash,
-	MAX(queryData.SQLStatement) AS SQLStatement,
-	MAX(queryData.creation_time) AS creation_time,
+	queryData.plan_handle,
+	queryData.SQLStatement AS SQLStatement,
+	queryData.creation_time AS creation_time,
 	SUM(queryData.execution_count) AS execution_count,
 	SUM(queryData.total_logical_writes) AS total_logical_writes,
 	SUM(queryData.total_logical_reads) AS total_logical_reads,
 	SUM(queryData.total_physical_reads) AS total_physical_reads
 	FROM (
 		SELECT 
-			qs.sql_handle,
-			qs.query_hash,
+			qs.plan_handle,
 			SUBSTRING(
 						st.TEXT, 
 						(qs.statement_start_offset / 2) + 1, 
@@ -43,13 +51,13 @@ SELECT
 			qs.total_logical_reads,
 			qs.total_physical_reads
 		FROM sys.dm_exec_query_stats AS qs
-		CROSS APPLY sys.dm_exec_sql_text(qs.sql_handle) AS st) AS queryData
-GROUP BY queryData.sql_handle, queryData.query_hash
-ORDER BY queryData.sql_handle, queryData.query_hash
+		CROSS APPLY sys.dm_exec_sql_text(qs.sql_handle) AS st
+		WHERE st.encrypted = 0) AS queryData
+GROUP BY queryData.plan_handle, queryData.SQLStatement, queryData.creation_time
+ORDER BY queryData.plan_handle, queryData.SQLStatement, queryData.creation_time
 
 SELECT 
-	sql_handle AS SqlHandle,
-	query_hash AS QueryHash,
+	plan_handle AS plan_handle,
 	SqlStatement,
 	creation_time AS CreationTime,
 	execution_count AS ExecutionCount,
@@ -58,19 +66,19 @@ SELECT
 	--total_physical_reads,
 	case 
 		when 
-			SqlStatement like '%INSERT%' 
-			or SqlStatement like '%UPDATE%'
-			or SqlStatement like '%INTO%'
-			or SqlStatement like '%DELETE%'
-			or SqlStatement like '%MERGE%'
-			or SqlStatement like '%WRITE%'
+			SqlStatement like '%INSERT %' 
+			or SqlStatement like '%UPDATE %'
+			or SqlStatement like '%INTO %'
+			or SqlStatement like '%DELETE %'
+			or SqlStatement like '%MERGE %'
+			or SqlStatement like '%WRITE %'
 		then 'Writes'
 		when 
-			SqlStatement like '%SELECT%'
-			or SqlStatement like '%READ%' 
+			SqlStatement like '%SELECT %'
+			or SqlStatement like '%READ %' 
 		then 'Reads'
 		when (total_logical_writes) > 0 then 'Writes'
 		when (total_logical_reads + total_physical_reads) > 0 then 'Reads'
-		else 'Unknown'
+		else 'Reads'
 	end as QueryType
 FROM @SqlText
