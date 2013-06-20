982fbd0072f69f549062e7eb41501a3134261017
Mike Merrill <mmerrill@crosscurrentinc.com>
2013-06-19 18:31:11 -0700

    Truncated the SQL text to speed up query

--- src/NewRelic.Microsoft.SqlServer.Plugin/Queries/SqlDMLActivity.SqlServerAndAzureSQLWithHash.sql
+++ src/NewRelic.Microsoft.SqlServer.Plugin/Queries/SqlDMLActivity.SqlServerAndAzureSQLWithHash.sql
@@ -6,18 +6,19 @@
 -- So, we need to track SQL statements in plans and add up the stats since compile time.
 -- Encrypted SQL statements are ignored
 -- We default to a Read when the statement is not categorized. (rare)
+-- To speed up execution, we only look at the first 50 chars of the SQL Text. This will allow some statements to have a misleading QueryType.
 -- Use: 
 	--plan_handle, SQLStatement and creation_time will give a unique item to calc delta. 
 	--These stay in the result set until they un-cache. 
 	--Un-cached items count as a 0 delta. 
 	--Drop these from delta tracking list.
-	--Just to be safe, please group the results by PlanHandle, SQL, Creation Time and Query Type
+	--Just to be safe, please group the results by PlanHandle, SqlStatementHash, CreationTime and QueryType
 
 
 
 declare @SqlText as table(
 	plan_handle varbinary(64),
-	SqlStatement nvarchar(max),
+	SqlStatement nvarchar(50),
 	creation_time DATETIME,
 	execution_count BIGINT,
 	total_logical_writes BIGINT,
@@ -38,14 +39,14 @@ SELECT
 	FROM (
 		SELECT 
 			qs.plan_handle,
-			SUBSTRING(
+			LEFT(SUBSTRING(
 						st.TEXT, 
 						(qs.statement_start_offset / 2) + 1, 
 						((	CASE qs.statement_end_offset
 								WHEN -1 THEN DATALENGTH(st.TEXT) 
 								ELSE qs.statement_end_offset
 							END - qs.statement_start_offset) / 2) + 1
-			) AS SQLStatement,
+			),50) AS SQLStatement,
 			qs.creation_time,
 			qs.execution_count,
 			qs.total_logical_writes,
@@ -60,7 +61,8 @@ ORDER BY queryData.plan_handle, queryData.SQLStatement, queryData.creation_time;
 
 SELECT 
 	plan_handle AS PlanHandle,
-	hashbytes('MD2', LEFT(SqlStatement,4000)) AS SqlStatementHash,
+	hashbytes('MD2', SqlStatement) AS SqlStatementHash,
+	SqlStatement,
 	creation_time AS CreationTime,
 	execution_count AS ExecutionCount,
 	--total_logical_writes,
@@ -69,14 +71,14 @@ SELECT
 	case 
 		WHEN
 			( 
-			SqlStatement like '%INSERT %' 
+			SqlStatement like '%INSERT%' 
 			or SqlStatement like '%UPDATE %'
 			or SqlStatement like '%INTO %'
-			or SqlStatement like '%DELETE %'
+			or SqlStatement like '%DELETE%'
 			or SqlStatement like '%MERGE %'
 			or SqlStatement like '%WRITE %'
 			)
-			AND SqlStatement NOT LIKE '%Exclude this query%' --Exclude this query from writes
+			AND SqlStatement NOT LIKE '%plan_handle AS PlanHandle%' --Exclude this query from writes
 		then 'Writes'
 		when 
 			SqlStatement like '%SELECT %'
