ec45eaeafd17f701f575546ea65dfe1ba337c5ff
Jesse Stromwick <jstromwick@gmail.com>
2013-09-08 20:46:35 -0700

    Including updated versions of the .NET Platform SDK, which should now have better, easier to use support for proxy servers! Yay!
    Added documentation (to our config that gets deployed) with instructions how to setup the configuration to work with proxy servers,
    both when the default credentials are needed, or when further credentials must be specified.

--- src/NewRelic.Microsoft.SqlServer.Plugin.Tests/Configuration/Log4NetTests.cs
+++ src/NewRelic.Microsoft.SqlServer.Plugin.Tests/Configuration/Log4NetTests.cs
@@ -1,20 +1,20 @@
-﻿using NUnit.Framework;
+﻿using log4net;
 
 using NewRelic.Microsoft.SqlServer.Plugin.Properties;
 
-using log4net;
+using NUnit.Framework;
 
 namespace NewRelic.Microsoft.SqlServer.Plugin.Configuration
 {
-    [TestFixture]
-    public class Log4NetTests
-    {
-        [Test]
-        public void AssertCanFindLog4NetConfig()
-        {
-            Program.SetUpLogConfig();
-            var logger = LogManager.Exists(Constants.SqlMonitorLogger);
-            Assert.That(logger, Is.Not.Null, "Couldn't Find Logger with name '{0}'", Constants.SqlMonitorLogger);
-        }
-    }
+	[TestFixture]
+	public class Log4NetTests
+	{
+		[Test]
+		public void AssertCanFindLog4NetConfig()
+		{
+			Program.SetUpLogConfig();
+			ILog logger = LogManager.Exists(Constants.SqlMonitorLogger);
+			Assert.That(logger, Is.Not.Null, "Couldn't Find Logger with name '{0}'", Constants.SqlMonitorLogger);
+		}
+	}
 }
