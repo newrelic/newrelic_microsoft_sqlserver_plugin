1fcbf768ec3d85b514bb6b11479321ddc5634ab2
Adam Larson <alarson@newrelic.com>
2014-09-19 16:44:59 -0700

    Release 2.0.1

--- src/NewRelic.Microsoft.SqlServer.Plugin/Configuration/Settings.cs
+++ src/NewRelic.Microsoft.SqlServer.Plugin/Configuration/Settings.cs
@@ -82,7 +82,7 @@ namespace NewRelic.Microsoft.SqlServer.Plugin.Configuration
         private static IWebProxy GetWebProxy(NewRelicConfiguration config)
         {
             var proxyElement = config.Proxy;
-            if (proxyElement == null) return null;
+            if (proxyElement == null || string.IsNullOrEmpty(proxyElement.Host)) return null;
 
             Uri uri;
             if (!Uri.TryCreate(proxyElement.Host, UriKind.RelativeOrAbsolute, out uri))
